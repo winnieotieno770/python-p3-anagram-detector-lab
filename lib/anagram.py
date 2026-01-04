4caed40110dc22e2e1105c4a15d9924bcca4c0a7
winnieotieno770 <winnieotieno770@gmail.com>
2026-01-04 16:16:21 +0300

    fix(anagram): Implement sorting logic ans case insesitive matching

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram():
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self,list):
+        matches = []
+        targeted_lowered = sorted(self.word)
+
+        for word in list:
+            lowered_word = word.lower()
+            if sorted(lowered_word) == targeted_lowered:
+                matches.append(lowered_word)
+        
+            
+        return matches
